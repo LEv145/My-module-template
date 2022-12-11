d44ca422f36bac91fe6fcd4043bc424ddabd6852
LEv145 <you@example.com>
2022-12-11 13:54:35 +0200

    v 0.1.2
    Add support of change LICENSE

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import typing as t
+import shutil
+from pathlib import Path
+
+PROJECT_DIRECTORY = Path(".").absolute()
+
+
+def remove(filepath: str) -> None:
+    target = PROJECT_DIRECTORY / filepath
+
+    if target.is_dir():
+        shutil.rmtree(target, ignore_errors=True)
+    else:
+        target.unlink()
+
+
+if __name__ == "__main__":
+    if "{{cookiecutter.open_source_license}}" == "Not open source":
+        remove("LICENSE")
